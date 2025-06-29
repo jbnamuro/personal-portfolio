e8459b660292c43fefa24fe30fd33ed83989e2f4
jbnamuro <jbnamuro12@gmail.com>
2025-06-29 10:12:29 -0400

    screen

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -174,7 +174,7 @@ const Header = () => {
       ref={menuCont}
       className="fixed top-0 left-0 w-full z-100 flex justify-between px-5 py-2 text-xl"
     >
-      <div className="h-[100svh] bg-black w-full absolute top-0 left-0 menu invisible">
+      <div className="h-screen bg-black w-full absolute top-0 left-0 menu invisible">
         <div className="h-full flex flex-col">
           <p
             className="underline px-5 py-2 self-end cursor-pointer"
