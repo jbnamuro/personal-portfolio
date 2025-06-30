9acb3dd1f617e69d4667c6f11c825f897d7307e7
jbnamuro <jbnamuro12@gmail.com>
2025-06-30 14:37:06 -0400

    fixes

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -66,33 +66,35 @@ const Landing = () => {
     <div ref={homeRef}>
       <div className="landing-cont flex flex-col justify-center items-center top-0 left-0 h-svh -z-1 w-full">
         <div className="w-full h-full bg-[url(/background.jpg)] bg-cover bg-no-repeat absolute top-0 left-0 -z-1 opacity-50"></div>
-        <div className="bg-white white-box scale-this text-[3.5vw]/[3.5vw] opacity-0 md:text-2xl p-4 aspect-5/6  max-h-100 max-w-150 flex font-light flex-col justify-between w-[75%] text-black">
-          <div className="opacity-50 flex justify-between">
-            <p className="">Jabari Namuro</p>
-            <p>Based in Canada</p>
-          </div>
-          <div>
-            <p>Digital Developer/Designer</p>
-            <div className="relative w-fit">
-              <h1 className="font-medium text-nowrap text-[7.5vw]/[7.5vw] md:text-6xl">
-                Purposeful Design <br />
-                Beautiful Aesthetics
-              </h1>
-              <img
-                src="/flower.svg"
-                className="absolute right-[2.5vw] -top-[1vw] md:right-[25px] md:-top-[10px] max-w-[25px] w-[3.5vw]"
-              />
+        <div className="w-full h-full flex justify-center items-center flex-col scale-this ">
+          <div className="bg-white white-box text-[3.5vw]/[3.5vw] opacity-0 md:text-2xl p-4 aspect-5/6  max-h-100 max-w-150 flex font-light flex-col justify-between w-[75%] text-black">
+            <div className="opacity-50 flex justify-between">
+              <p className="">Jabari Namuro</p>
+              <p>Based in Canada</p>
+            </div>
+            <div>
+              <p>Digital Developer/Designer</p>
+              <div className="relative w-fit">
+                <h1 className="font-medium text-nowrap text-[7.5vw]/[7.5vw] md:text-6xl">
+                  Purposeful Design <br />
+                  Beautiful Aesthetics
+                </h1>
+                <img
+                  src="/flower.svg"
+                  className="absolute right-[2.5vw] -top-[1vw] md:right-[25px] md:-top-[10px] max-w-[25px] w-[3.5vw]"
+                />
+              </div>
             </div>
           </div>
-        </div>
-        <div className="text-center mt-10 bottom-2 scale-this w-[70%] landing-info max-w-150">
-          <p className="font-bold text-[3vw] md:text-xl opacity-0 land-text1">
-            CRAFTING ANIMATED, INTERACTIVE WEB EXPERIENCES THAT ELEVATE BRANDS
-            AND CAPTIVATE USERS.
-          </p>
-          <p className="mt-2 text-[3vw] md:text-xl land-text2 opacity-0">
-            (scroll)
-          </p>
+          <div className="text-center mt-10 bottom-2 w-[70%] landing-info max-w-150">
+            <p className="font-bold text-[3vw] md:text-xl opacity-0 land-text1">
+              CRAFTING ANIMATED, INTERACTIVE WEB EXPERIENCES THAT ELEVATE BRANDS
+              AND CAPTIVATE USERS.
+            </p>
+            <p className="mt-2 text-[3vw] md:text-xl land-text2 opacity-0">
+              (scroll)
+            </p>
+          </div>
         </div>
       </div>
     </div>
