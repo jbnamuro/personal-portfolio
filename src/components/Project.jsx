5baf2fcab247b0acf76e311d17072b2b13c8fd46
jbnamuro <jbnamuro12@gmail.com>
2026-03-22 23:07:43 -0400

    changes

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const Project = (props) => {
   return (
-    <div className="border-r-1 border-y-1 border-[rgba(255,255,255,0.3)] h-150 min-w-[500px] flex flex-col justify-center items-center">
-      <img src={props.image} alt={props.title} className="h-2/3 invert" />
-      <div className="bg-red-500 w-full px-5 h-1/3 py-5">
+    <div className="border-r-1 border-y-1 border-[rgba(255,255,255,0.3)] h-120 min-w-100 flex flex-col justify-center items-center">
+      <div className="h-1/2 flex justify-center w-full items-center bg-blue-500"><img src={props.image} alt={props.title} className="invert w-1/4" /></div>
+      <div className="bg-red-500 w-full px-5 h-1/2 py-5">
         <h2 className="text-3xl mb-2">{props.title}</h2>
-        <p className="mb-12">{props.description}</p>
+        <p className="mb-19">{props.description}</p>
         <button className="cursor-pointer rounded-3xl border border-white px-4 py-2">
           Learn More
         </button>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,6 +7,13 @@ import useFont from "./useFont";
 import { SplitText } from "gsap/SplitText";
 import Project from "./Project";
 
+const projects = [
+  { id: "p1", title: "Project 1", description: "Description of Project 1", image: "/thingy.svg" },
+  { id: "p2", title: "Project 2", description: "Description of Project 2", image: "/thingy.svg" },
+  { id: "p3", title: "Project 3", description: "Description of Project 3", image: "/thingy.svg" },
+  { id: "p4", title: "Project 4", description: "Description of Project 4", image: "/thingy.svg" },
+];
+
 const Projects = () => {
   const font = useFont();
   const projectRef = useRef(null);
@@ -16,10 +23,22 @@ const Projects = () => {
 
   return (
     <div className="h-fit-content w-full bg-black text-white overflow-hidden">
+      <div className="px-5 py-10 flex justify-between items-center">
+        <h1 className="text-4xl">Selected Works</h1>
+        <div className="bg-red-500 h-2 w-5">
+          <div></div>
+          <div></div>
+        </div>
+      </div>
       <div className="boxes flex">
-        <Project title="Project 1" description="Description of Project 1" image="/thingy.svg"/>
-        <Project title="Project 2" description="Description of Project 2" image="/thingy.svg"/>
-        <Project title="Project 3" description="Description of Project 3" image="/thingy.svg"/>
+        {projects.map((project) => (
+          <Project
+            key={project.id}
+            title={project.title}
+            description={project.description}
+            image={project.image}
+          />
+        ))}
       </div>
     </div>
   );
