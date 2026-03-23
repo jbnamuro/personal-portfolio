c2b94cb6710bda8e699cdedd29cf41ce69275c8f
jbnamuro <jbnamuro12@gmail.com>
2026-03-23 04:38:07 -0400

    about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -90,8 +90,8 @@ const About = () => {
       </div>
       <p className="pt-[28px] md:pt-[30px] lg:pt-[34px] xl:pt-[40px] abt text-4xl md:text-5xl lg:text-6xl xl:text-7xl mt-2">
         I'm Jabari, a developer and designer who builds interactive,
-        high-performing web experiences from the ground up. I care about both
-        the code under the hood and the details users actually see and feel.
+        high-performing programs from the ground up. I care about both the code
+        under the hood and the details users actually see and feel.
       </p>
       <p className="font-light mt-5 met opacity-50 text-[3vw] md:text-lg lg:text-xl xl:text-2xl">
         Meticulously crafting
